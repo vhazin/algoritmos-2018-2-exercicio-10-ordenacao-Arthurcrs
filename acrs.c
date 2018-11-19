4eedccc33b96b111a15a86b6815d988f28a98f48
acrs <acrs@cesar.school>
2018-11-19 09:52:04 -0300

    first commit to test

---
+++ acrs.c
@@ -0,0 +1,51 @@
+/*
+Arthur Carlos da Rocha Silva
+
+Ordenando a Lista de Crianças do Papai Noel
+https://www.urionlinejudge.com.br/judge/pt/problems/view/2479
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define NAME_MAX_NUMBER_OF_CHARACTERS 20
+
+int numberOfNames; //global variable
+
+void printArrayOfNames(char *inputArray)
+{
+    for (int i = 0; i < numberOfNames; i++)
+    {
+        printf("%s", inputArray[i]);
+    }
+    return;
+}
+
+int main()
+{
+    char behaviorCharacter;
+    int numberOfBadBehavior = 0;
+    int numberOfGoodBehavior = 0;
+    scanf("%d", &numberOfNames);
+    char *names[20] = (char *)malloc(numberOfNames * NAME_MAX_NUMBER_OF_CHARACTERS * sizeof(char));
+    for (int i = 0; i < numberOfNames; i++)
+    {
+        scanf("%c %s", &behaviorCharacter, &names[i]);
+        if (behaviorCharacter == '+')
+        {
+            numberOfGoodBehavior++;
+        }
+        else
+        {
+            numberOfBadBehavior++;
+        }
+        //store name in array of names
+    }
+    
+    //sort array of names
+    
+    printArrayOfNames(names);
+    printf("Se comportaram : %d | Nao se comportaram : %d", numberOfGoodBehavior, numberOfBadBehavior);
+    return 0;
+}
